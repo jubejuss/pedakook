66e8fd6f636e35876a145fc17658a8fb44922fee
Juho Kalberg <juhokalberg@Juhos-MacBook-Pro-2.local>
2023-09-09 20:49:12 +0300

    made first posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Infoõps blogib';
+export const SITE_DESCRIPTION = 'Sellel lehel teeb informaatika õpetaja märkmeid tööst ja õpimaterjalidest';
